8665ae906b72d099ec6dd6ee75e3693d8fd1f7de
Cam Pos <campostlethwaite@hotmail.com>
2018-04-24 22:10:35 +1000

    add methods to calculator.rb

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
-#write your code here
+# write your code here
+def add(num_one, num_two)
+  num_one + num_two
+end
+
+def subtract(num_one, num_two)
+  num_one - num_two
+end
+
+def sum(arr)
+  total = 0
+  arr.each { |num| total += num }
+  total
+end
+
+def multiply(arr)
+  total = arr[0]
+  for num in 1...arr.count
+    total *= arr[num]
+  end
+  total
+end
+
+def power(num_one, num_two)
+  num_one ** num_two
+end
+
+def factorial(num)
+  if num <= 1
+    1
+  else
+    num * factorial(num - 1)
+  end
+end
